be038c79537af56feb9a64b2c2fdbfd043d41420
euougo <euougo@gmail.com>
2021-06-16 16:48:33 -0300

    first final version

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -9,6 +9,7 @@ namespace dotnet_crud_dio
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+        private bool Excluido { get; set; }
 
         // Métodos
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
@@ -18,6 +19,7 @@ namespace dotnet_crud_dio
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -26,7 +28,8 @@ namespace dotnet_crud_dio
             retorno += "Gênero: " + this.Genero + Environment.NewLine;
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
-            retorno += "Ano de Início: " + this.Ano;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido;
             return retorno;
         }
 
@@ -38,6 +41,15 @@ namespace dotnet_crud_dio
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+
+        public void Excluir() {
+            this.Excluido = true;
         }
     }
 }
--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 ## Bootcamp .Net Fundamentals - Digital Innovation One
 
-parte 3
+![Projeto: Criando um APP simples de cadastro de séries em .NET ](./Capture.PNG "Cadastro de séries em .NET")
+
+---
+
+## .Net Fundamentals
+
+Esse bootcamp é para as pessoas que estão iniciando sua carreira em desenvolvimento de software. Você aprenderá a desenvolver projetos em uma das linguagens de back-end mais utilizadas no mundo, o .NET C#, e a resolver algoritmos tendo um aprendizado completo, da teoria à prática.
+
+<a href="https://digitalinnovation.one/"> <img src="./Digital-Innovation-One.png" alt="Digital Innovation One" width="200"></a>
